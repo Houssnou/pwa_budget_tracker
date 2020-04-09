21aadb2737885a6450f3fa6a219157cfadc96209
Houssnou <hosni.moubarak@gmail.com>
2020-04-09 18:36:48 -0400

    adding full pwa support

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const FILES_TO_CACHE = ["/", "/index.html", "/index.js", "/styles.css", "/db.js"];
+const FILES_TO_CACHE = ["/", "/index.html", "/index.js", "/styles.css", "/db.js", "/chart.js"];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
